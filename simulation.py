50fb7eca4a6ed41d2375feca703a79d0b2f04b87
Ryan Brand <UNA8949@gmail.com>
2017-12-12 13:33:20 -0700

    Bottleneck and different priorities for testing

--- simulation.py
+++ simulation.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     frwd_tbl_D = {'H2': {2: 1}, 'H1': {2: 0}, 'H3': {0: 2, 1: 2}}
     decap_tbl_D = {'H2': 0, 'H1': 0, 'H3': 1}
     router_d = Router(name='RD',
-              intf_capacity_L=[500,500,500],
+              intf_capacity_L=[500,500,100],
               encap_tbl_D = encap_tbl_D,
               frwd_tbl_D = frwd_tbl_D,
               decap_tbl_D = decap_tbl_D,
@@ -90,10 +90,9 @@ if __name__ == '__main__':
         t.start()
     
     #create some send events    
-    for i in range(1):
+    for i in range(2):
         priority = i%2
         host_1.udt_send('H3', 'MESSAGE_%d_FROM_H1' % i, priority)
-        host_2.udt_send('H3', 'MESSAGE_%d_FROM_H2' % i, priority)
         
     #give the network sufficient time to transfer all packets before quitting
     sleep(simulation_time)
